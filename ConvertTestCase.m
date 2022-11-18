752e1c416fbcc6cda13e59f325b8235f9ae5baf7
Seth Bashford <seth.bashford@boystown.org>
2022-11-18 14:09:12 -0600

    add to test

--- ConvertTestCase.m
+++ ConvertTestCase.m
@@ -2,8 +2,8 @@ classdef ConvertTestCase < matlab.unittest.TestCase
     methods (Test)
         function tbd(self)
             trial.correct = 1;
-            trial.responses = '[{"milliseconds":44204,"id":1},{"milliseconds":44897.89999997616,"id":2},{"milliseconds":45672.80000001192,"id":1}]';
             trial.simon = '[{"milliseconds":39390.09999996424,"id":1},{"milliseconds":40789.80000001192,"id":2},{"milliseconds":42190.39999997616,"id":1}]';
+            trial.responses = '[{"milliseconds":44204,"id":1},{"milliseconds":44897.89999997616,"id":2},{"milliseconds":45672.80000001192,"id":1}]';
             converted = convert(trial);
             self.assertEqual(numel(converted), 6);
             self.assertEqual(converted(1).responsetime, 39390.09999996424);
@@ -12,6 +12,12 @@ classdef ConvertTestCase < matlab.unittest.TestCase
             self.assertEqual(converted(4).responsetime, 44204);
             self.assertEqual(converted(5).responsetime, 44897.89999997616);
             self.assertEqual(converted(6).responsetime, 45672.80000001192);
+            self.assertEqual(converted(1).id, 1);
+            self.assertEqual(converted(2).id, 2);
+            self.assertEqual(converted(3).id, 1);
+            self.assertEqual(converted(4).id, 1);
+            self.assertEqual(converted(5).id, 2);
+            self.assertEqual(converted(6).id, 1);
         end
     end
 end
--- convert.m
+++ convert.m
@@ -1,11 +1,13 @@
 function converted = convert(trial)
 responses = jsondecode(trial.responses);
 simon = jsondecode(trial.simon);
-converted(numel(responses)+numel(simon)).tbd = true;
+converted(numel(responses)+numel(simon)).id = 0;
 for i = 1:numel(simon)
     converted(i).responsetime = simon(i).milliseconds;
+    converted(i).id = simon(i).id;
 end
 for i = 1:numel(responses)
     converted(i+numel(simon)).responsetime = responses(i).milliseconds;
+    converted(i+numel(simon)).id = responses(i).id;
 end
 end
