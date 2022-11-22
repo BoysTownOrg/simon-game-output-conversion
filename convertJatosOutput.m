c213144e78d3f6f7b58bac2f28daf89d53f03ea8
Seth Bashford <seth.bashford@boystown.org>
2022-11-22 14:22:21 -0600

    refactor

--- convertJatosOutput.m
+++ convertJatosOutput.m
@@ -5,26 +5,28 @@ if isequal(file, 0) || isequal(path, 0)
 end
 lines = readlines([path, filesep, file]);
 lines(lines.strlength == 0) = [];
-json = arrayfun(@(s)jsondecode(s), lines, 'UniformOutput', false);
-for task = 1:numel(json)
-    trials = json{task};
+tasks = arrayfun(@(s)jsondecode(s), lines, 'UniformOutput', false);
+for i = 1:numel(tasks)
+    trials = tasks{i};
+    taskName = '?';
+    participantID = '?';
+    outputTable = table();
     try
-        assert(numel(trials) > 2, "Task does not have more than two trials.");
-        if isfield(trials{3}, "response_key")
-            taskName = 'squares';
-        elseif isfield(trials{3}, "block")
-            taskName = 'colors';
-        else
-            taskName = 'practice';
-        end
-        assert(isfield(trials{1}, "response") && isfield(trials{1}.response, "participant_id"), "Task does not have participant ID");
-        participantID = trials{1}.response.participant_id;
-        outputTable = table();
-        for i = 3:numel(trials)
-            if isfield(trials{i}, "simon")
-                trialTable = struct2table(convert(trials{i}));
+        for j = 1:numel(trials)
+            if isfield(trials{j}, "response") && isfield(trials{j}.response, "participant_id")
+                participantID = trials{j}.response.participant_id;
+            end
+            if isfield(trials{j}, "simon")
+                if isfield(trials{j}, "response_key")
+                    taskName = 'squares';
+                elseif isfield(trials{j}, "block")
+                    taskName = 'colors';
+                else
+                    taskName = 'practice';
+                end
+                trialTable = struct2table(convert(trials{j}), 'AsArray', true);
                 participant_id = repmat(string(participantID), height(trialTable), 1);
-                trial = repmat(i - 2, height(trialTable), 1);
+                trial = repmat(j - 2, height(trialTable), 1);
                 outputTable = [outputTable; addvars(trialTable, participant_id, trial)];
             end
         end
@@ -34,8 +36,8 @@ for task = 1:numel(json)
         end
         writetable(outputTable, outputFile);
     catch ME
-        warning(['Failed to write task #', num2str(task), '.']);
-        warning(ME.message);
+        warning(['Failed to write ', taskName, ' task for participant ', participantID, '.']);
+        disp(ME.message);
     end
 end
 end
